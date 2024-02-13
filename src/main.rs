fa6a022074b88ef00496587357497862a683d253
Eyepatch-13 <toranjain07@gmail.com>
2024-02-13 11:06:39 +0530

    Added special fields in PieceType:{King, Rook, Pawn} to account for situtations regarding checks, castles and pawn initial 2 cell move

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,53 @@ trait Movement {
 
 #[derive(Clone, Debug)]
 enum PieceType {
-    King, Queen, Bishop, Knight, Rook, Pawn,
+    King {
+        in_check: bool,
+        has_moved: bool,
+        is_checkmated: bool,
+    }, Queen, Bishop, Knight
+    , Rook {
+        has_moved: bool,
+    }
+    , Pawn {
+        has_moved: bool,
+    },
+}
+
+impl PieceType {
+    fn new(piece: &str) -> Self {
+        match piece {
+            "King" => {
+                return PieceType::King {
+                    is_checkmated: false,
+                    in_check: false,
+                    has_moved: false,
+                };
+            },
+            "Queen" => {
+                return PieceType::Queen;
+            },
+            "Bishop" => {
+                return PieceType::Bishop;
+            },
+            "Knight" => {
+                return PieceType::Knight;
+            },
+            "Rook" => {
+                return PieceType::Rook {
+                    has_moved: false,
+                };
+            },
+            "Pawn" => {
+                return PieceType::Pawn {
+                    has_moved: false
+                };
+            },
+            _ => {
+                panic!("Not a Piece Type or Incorrect Spelling \" Eg- Pawn and not pawn\"");
+            }
+        }
+    }
 }
 
 #[derive(Clone, Debug)]
@@ -40,7 +86,7 @@ impl Piece {
 
 impl Movement for Piece {
     fn moves(&self) {
-        todo!()
+        todo!();
     }
 }
 
@@ -51,27 +97,27 @@ struct Board {
 
 impl Board {
     fn new() -> Self {
-        let wpawn: Vec<Option<Piece>> = vec![Some(Piece::new(PieceType::Pawn, PieceColor::White)); 8];
-        let bpawn: Vec<Option<Piece>> = vec![Some(Piece::new(PieceType::Pawn, PieceColor::Black)); 8];
+        let wpawn: Vec<Option<Piece>> = vec![Some(Piece::new(PieceType::new("Pawn"), PieceColor::White)); 8];
+        let bpawn: Vec<Option<Piece>> = vec![Some(Piece::new(PieceType::new("Pawn"), PieceColor::Black)); 8];
         let blank: Vec<Option<Piece>> = vec![None; 8];
         let mut wpiece: Vec<Option<Piece>> = vec![None; 8];
         let mut bpiece = wpiece.clone();
-        wpiece[0] = Some(Piece::new(PieceType::Rook, PieceColor::White));
-        wpiece[1] = Some(Piece::new(PieceType::Knight, PieceColor::White));
-        wpiece[2] = Some(Piece::new(PieceType::Bishop, PieceColor::White));
-        wpiece[3] = Some(Piece::new(PieceType::Queen, PieceColor::White));
-        wpiece[4] = Some(Piece::new(PieceType::King, PieceColor::White));
-        wpiece[5] = Some(Piece::new(PieceType::Bishop, PieceColor::White));
-        wpiece[6] = Some(Piece::new(PieceType::Knight, PieceColor::White));
-        wpiece[7] = Some(Piece::new(PieceType::Rook, PieceColor::White));
-        bpiece[0] = Some(Piece::new(PieceType::Rook, PieceColor::Black));
-        bpiece[1] = Some(Piece::new(PieceType::Knight, PieceColor::Black));
-        bpiece[2] = Some(Piece::new(PieceType::Bishop, PieceColor::Black));
-        bpiece[3] = Some(Piece::new(PieceType::Queen, PieceColor::Black));
-        bpiece[4] = Some(Piece::new(PieceType::King, PieceColor::Black));
-        bpiece[5] = Some(Piece::new(PieceType::Bishop, PieceColor::Black));
-        bpiece[6] = Some(Piece::new(PieceType::Knight, PieceColor::Black));
-        bpiece[7] = Some(Piece::new(PieceType::Rook, PieceColor::Black));
+        wpiece[0] = Some(Piece::new(PieceType::new("Rook"), PieceColor::White));
+        wpiece[1] = Some(Piece::new(PieceType::new("Knight"), PieceColor::White));
+        wpiece[2] = Some(Piece::new(PieceType::new("Bishop"), PieceColor::White));
+        wpiece[3] = Some(Piece::new(PieceType::new("Queen"), PieceColor::White));
+        wpiece[4] = Some(Piece::new(PieceType::new("King"), PieceColor::White));
+        wpiece[5] = Some(Piece::new(PieceType::new("Bishop"), PieceColor::White));
+        wpiece[6] = Some(Piece::new(PieceType::new("Knight"), PieceColor::White));
+        wpiece[7] = Some(Piece::new(PieceType::new("Rook"), PieceColor::White));
+        bpiece[0] = Some(Piece::new(PieceType::new("Rook"), PieceColor::Black));
+        bpiece[1] = Some(Piece::new(PieceType::new("Knight"), PieceColor::Black));
+        bpiece[2] = Some(Piece::new(PieceType::new("Bishop"), PieceColor::Black));
+        bpiece[3] = Some(Piece::new(PieceType::new("Queen"), PieceColor::Black));
+        bpiece[4] = Some(Piece::new(PieceType::new("King"), PieceColor::Black));
+        bpiece[5] = Some(Piece::new(PieceType::new("Bishop"), PieceColor::Black));
+        bpiece[6] = Some(Piece::new(PieceType::new("Knight"), PieceColor::Black));
+        bpiece[7] = Some(Piece::new(PieceType::new("Rook"), PieceColor::Black));
         Board {
             grid: [bpiece, bpawn, blank.clone(), blank.clone(), blank.clone(), blank.clone(), wpawn, wpiece].to_vec(),
         }
@@ -102,18 +148,18 @@ impl EventHandler for Board {
                 let path = match &self.grid[j][i] {
                     Some(piece) => {
                         match (&piece.piece_type, &piece.piece_color) {
-                            (PieceType::King, PieceColor::White) => {"/white-king.png"},
+                            (PieceType::King {is_checkmated, in_check, has_moved}, PieceColor::White) => {"/white-king.png"},
                             (PieceType::Queen, PieceColor::White) => {"/white-queen.png"},
                             (PieceType::Bishop, PieceColor::White) => {"/white-bishop.png"},
                             (PieceType::Knight, PieceColor::White) => {"/white-knight.png"},
-                            (PieceType::Rook, PieceColor::White) => {"/white-rook.png"},
-                            (PieceType::Pawn, PieceColor::White) => {"/white-pawn.png"},
-                            (PieceType::King, PieceColor::Black) => {"/black-king.png"},
+                            (PieceType::Rook {has_moved}, PieceColor::White) => {"/white-rook.png"},
+                            (PieceType::Pawn {has_moved}, PieceColor::White) => {"/white-pawn.png"},
+                            (PieceType::King {is_checkmated, in_check, has_moved}, PieceColor::Black) => {"/black-king.png"},
                             (PieceType::Queen, PieceColor::Black) => {"/black-queen.png"},
                             (PieceType::Bishop, PieceColor::Black) => {"/black-bishop.png"},
                             (PieceType::Knight, PieceColor::Black) => {"/black-knight.png"},
-                            (PieceType::Rook, PieceColor::Black) => {"/black-rook.png"},
-                            (PieceType::Pawn, PieceColor::Black) => {"/black-pawn.png"},
+                            (PieceType::Rook {has_moved}, PieceColor::Black) => {"/black-rook.png"},
+                            (PieceType::Pawn {has_moved}, PieceColor::Black) => {"/black-pawn.png"},
                         }
                     },
                     None => continue,
